3e17ca9e46f75e62942d83718ce6ee93c8419a37
Wenzhuo Liu <mgt@oi-wiki.org>
2024-04-02 18:03:48 +0800

    fix: lines in block math equation is indented (#42)
    * fix: first line in block equation not indented
    
    * test: add complex math testcase
    
    * no indent for math block

--- src/pretty/mod.rs
+++ src/pretty/mod.rs
@@ -330,7 +330,7 @@ impl PrettyPrinter {
         if equation.block() {
             doc = doc.append(BoxDoc::line());
         }
-        doc = doc.append(self.convert_math(equation.body()).nest(2));
+        doc = doc.append(self.convert_math(equation.body()));
         if equation.block() {
             doc = doc.append(BoxDoc::line());
         }
