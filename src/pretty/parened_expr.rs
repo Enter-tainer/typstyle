68f34d0b9bc51e1a37512c6559dedc9b3dc6a7b6
mgt <mgt@oi-wiki.org>
2024-07-13 12:55:08 +0800

    fix: when iter target in `for` is break into multiple lines, add paren around it

--- src/pretty/parened_expr.rs
+++ src/pretty/parened_expr.rs
@@ -23,7 +23,22 @@ impl PrettyPrinter {
         doc
     }
 
+    /// Convert an expression with optional parentheses.
+    /// If the expression is a parenthesized expression, a code block, a content block, or a function call,
+    /// the expression will be converted without parentheses.
+    /// Otherwise, the expression will be converted with parentheses if it is layouted on multiple lines.
     pub(super) fn convert_expr_with_optional_paren<'a>(&'a self, expr: Expr<'a>) -> BoxDoc<'a, ()> {
+        if matches!(
+            expr,
+            Expr::Parenthesized(_)
+                | Expr::Code(_)
+                | Expr::Content(_)
+                | Expr::FuncCall(_)
+                | Expr::Array(_)
+                | Expr::Dict(_)
+        ) {
+            return self.convert_expr(expr);
+        }
         let left_paren_or_nil = BoxDoc::text("(")
             .append(BoxDoc::line())
             .flat_alt(BoxDoc::nil());
