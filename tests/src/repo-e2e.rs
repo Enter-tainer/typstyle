b2e1453fb4a4e11afd80c36eca143446569c96f5
QuadnucYard <quadnucyard@gmail.com>
2025-04-22 11:38:36 +0800

    dev: move config of e2e repos to toml file

--- tests/src/repo-e2e.rs
+++ tests/src/repo-e2e.rs
@@ -1,147 +1,31 @@
-use std::{borrow::Cow, collections::HashSet, fs, path::Path};
+use std::{collections::HashSet, fs, path::Path};
 
 use anyhow::Context;
 use libtest_mimic::{Failed, Trial};
+use serde::Deserialize;
 use typst_syntax::Source;
 use typstyle_consistency::{cmp::compare_docs, universe::make_universe_formatted};
 use typstyle_core::Typstyle;
 
-use crate::common::test_dir;
+use crate::common::{fixtures_dir, test_dir};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Deserialize)]
 struct Testcase {
-    name: Cow<'static, str>,
-    repo_url: Cow<'static, str>,
-    revision: Cow<'static, str>,
-    entrypoint: Cow<'static, str>,
+    name: String,
+    git: String,
+    rev: String,
+    entrypoint: String,
     // these files are included as-is and should not be formatted
+    #[serde(default)]
     blacklist: HashSet<String>,
 }
 
-impl Testcase {
-    fn new(
-        name: impl Into<Cow<'static, str>>,
-        repo_url: impl Into<Cow<'static, str>>,
-        revision: impl Into<Cow<'static, str>>,
-        entrypoint: impl Into<Cow<'static, str>>,
-    ) -> Self {
-        Self {
-            name: name.into(),
-            repo_url: repo_url.into(),
-            revision: revision.into(),
-            entrypoint: entrypoint.into(),
-            blacklist: HashSet::new(),
-        }
-    }
-
-    fn with_blacklist(mut self, blacklist: impl IntoIterator<Item = impl Into<String>>) -> Self {
-        self.blacklist = blacklist.into_iter().map(Into::into).collect();
-        self
-    }
-}
-
 pub(super) fn collect_tests() -> Vec<Trial> {
-    let cases = [
-        Testcase::new(
-            "tutorial",
-            "https://github.com/typst-doc-cn/tutorial",
-            "3e1dcc83ca7abb314f84e985e84c15299790aaa8",
-            "src/ebook.typ",
-        ),
-        Testcase::new(
-            "uniquecv",
-            "https://github.com/gaoachao/uniquecv-typst",
-            "38fd15c5156e683f989fcf5a04b119b8a1d22f2e",
-            "main.typ",
-        ),
-        Testcase::new(
-            "cetz-manual",
-            "https://github.com/cetz-package/cetz",
-            "8ec63845f87941d8d6b90b5d0dd52ed4e74c3694",
-            "manual.typ",
-        ),
-        Testcase::new(
-            "HomotopyHistory",
-            "https://github.com/trebor-Huang/HomotopyHistory/",
-            "5ee6ff5f9b3e1dccae3b84cfb093cf5d649bc12c",
-            "paper.typ",
-        ),
-        Testcase::new(
-            "typst-talk",
-            "https://github.com/OrangeX4/typst-talk",
-            "742a0c614c0163dee557b101fb8e4e4063d51fd3",
-            "main.typ",
-        )
-        .with_blacklist(["chicv.typ"]),
-        Testcase::new(
-            "touying-example",
-            "https://github.com/touying-typ/touying",
-            "e2e20b7243733e14cfb303f3d22988e862655041",
-            "examples/example.typ",
-        ),
-        Testcase::new(
-            "tablex-test",
-            "https://github.com/PgBiel/typst-tablex",
-            "940d13c570f241a8a9de7512c453deaee29952e5",
-            "tablex-test.typ",
-        ),
-        Testcase::new(
-            "fletcher-manual",
-            "https://github.com/Jollywatt/typst-fletcher",
-            "a011539846850ad466f16fde715ab6f83a6512f4",
-            "docs/manual.typ",
-        )
-        // tidy has weird behavior when parsing typ source code
-        .with_blacklist(["main.typ", "marks.typ"]),
-        Testcase::new(
-            "nju-thesis-typst",
-            "https://github.com/nju-lug/nju-thesis-typst",
-            "8b481bddd3bfa683a5af2e22922e222d8d5d0f81",
-            "thesis.typ",
-        ),
-        Testcase::new(
-            "physica",
-            "https://github.com/Leedehai/typst-physics",
-            "443c963013b5cfea64818fa71990fefde9c93131",
-            "physica-manual.typ",
-        ),
-        Testcase::new(
-            "lovelace",
-            "https://github.com/andreasKroepelin/lovelace",
-            "a83b64662b1a6f78593b8e028e9a8162f1793d4c",
-            "examples/doc.typ",
-        ),
-        Testcase::new(
-            "quill",
-            "https://github.com/Mc-Zen/quill",
-            "3cd5f656c3c6845e267621d1d118d6c8f7731f37",
-            "docs/guide/quill-guide.typ",
-        )
-        .with_blacklist([
-            "shor-nine-qubit-code.typ",
-            "teleportation.typ",
-            "phase-estimation.typ",
-            "qft.typ",
-            "fault-tolerant-measurement.typ",
-            "fault-tolerant-pi8.typ",
-            "fault-tolerant-toffoli1.typ",
-            "fault-tolerant-toffoli2.typ",
-            "quill-guide.typ",
-        ]),
-        Testcase::new(
-            "curryst",
-            "https://github.com/pauladam94/curryst",
-            "1ffd5f41a22cf3a2ea1d48f65c959c1883fe08b3",
-            "examples/natural-deduction.typ",
-        ),
-        Testcase::new(
-            "derive-it",
-            "https://github.com/0rphee/derive-it",
-            "e56a25def12082b8c9c6d54026c426434a5610e8",
-            "examples/example.typ",
-        ),
-    ];
-    cases
+    let testcases = toml::from_str::<Vec<Testcase>>(
+        &fs::read_to_string(fixtures_dir().join("e2e-repos.toml")).unwrap(),
+    )
+    .unwrap();
+    testcases
         .into_iter()
         .map(|case| {
             Trial::test(format!("{} - e2e", case.name.clone()), move || {
@@ -170,7 +54,7 @@ fn clone_testcase_repo(testcase: &Testcase, testcase_dir: &Path) -> anyhow::Resu
     // do git clone with submodule
     std::process::Command::new("git")
         .arg("clone")
-        .arg(&*testcase.repo_url)
+        .arg(&*testcase.git)
         .arg(testcase_dir)
         .arg("--recurse-submodules")
         .output()
@@ -178,7 +62,7 @@ fn clone_testcase_repo(testcase: &Testcase, testcase_dir: &Path) -> anyhow::Resu
     // do git checkout testcase.revision
     std::process::Command::new("git")
         .arg("checkout")
-        .arg(&*testcase.revision)
+        .arg(&*testcase.rev)
         .current_dir(testcase_dir)
         .output()
         .context("failed to checkout revision")?;
