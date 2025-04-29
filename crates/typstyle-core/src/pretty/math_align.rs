f322d0a624d4f299ba2ff827ee0f02af8537774b
QuadnucYard <quadnucyard@gmail.com>
2025-04-29 10:30:26 +0800

    feat: align multiline cells

--- crates/typstyle-core/src/pretty/math_align.rs
+++ crates/typstyle-core/src/pretty/math_align.rs
@@ -2,6 +2,8 @@ use pretty::DocAllocator;
 use typst_syntax::{ast::*, SyntaxKind, SyntaxNode};
 use unicode_width::UnicodeWidthStr;
 
+use crate::ext::StrExt;
+
 use super::{doc_ext::AllocExt, ArenaDoc, Context, PrettyPrinter};
 
 impl<'a> PrettyPrinter<'a> {
@@ -25,44 +27,65 @@ impl<'a> PrettyPrinter<'a> {
         let ctx = ctx.aligned();
         let aligned_elems = collect_aligned(math);
 
-        // render cells
+        let (printed, col_widths) = self.render_cells_in_aligned(ctx, aligned_elems)?;
+        let doc = self.print_aligned_cells(printed, col_widths);
+        Some(doc)
+    }
+
+    #[allow(clippy::type_complexity)]
+    fn render_cells_in_aligned(
+        &'a self,
+        ctx: Context,
+        aligned_elems: Vec<Vec<Vec<&'a SyntaxNode>>>,
+    ) -> Option<(Vec<Vec<(String, usize)>>, Vec<usize>)> {
         // column widths have already considered padding around separators
-        let (printed, col_widths) = {
-            let col_num = aligned_elems.iter().map(|row| row.len()).max().unwrap_or(0);
-            let mut col_widths = vec![0; col_num];
 
-            let mut printed = vec![];
-            for row in aligned_elems {
-                let mut row_doc = vec![];
-                for (j, col) in row.into_iter().enumerate() {
-                    let cell_doc = self.convert_math_children(ctx, col.into_iter());
-                    let mut rendered = String::new();
-                    cell_doc.render_fmt(200, &mut rendered).ok()?;
-                    if rendered.contains('\n') {
-                        return None; // linebreaks not supported yet
-                    }
-                    let cell_width = if rendered.is_empty() {
-                        0
-                    } else if j == 0 || j + 1 == col_num {
-                        rendered.width() + 1
+        let col_num = aligned_elems.iter().map(|row| row.len()).max().unwrap_or(0);
+        let mut col_widths = vec![0; col_num];
+
+        let mut printed = vec![];
+        for row in aligned_elems {
+            let mut row_doc = vec![];
+            for (j, col) in row.into_iter().enumerate() {
+                let cell_doc = self.convert_math_children(ctx, col.into_iter());
+                let mut rendered = String::new();
+                cell_doc.render_fmt(200, &mut rendered).ok()?;
+
+                let cell_width = if rendered.is_empty() {
+                    0
+                } else {
+                    let render_width = rendered
+                        .lines()
+                        .map(|line| line.trim_ascii().width())
+                        .max()
+                        .unwrap_or(0);
+                    if j == 0 || j + 1 == col_num {
+                        render_width + 1
                     } else {
-                        rendered.width() + 2
-                    };
-                    col_widths[j] = col_widths[j].max(cell_width);
-                    row_doc.push((rendered, cell_width));
-                }
-                printed.push(row_doc);
+                        render_width + 2
+                    }
+                };
+                col_widths[j] = col_widths[j].max(cell_width);
+                row_doc.push((rendered, cell_width));
             }
-            (printed, col_widths)
-        };
+            printed.push(row_doc);
+        }
 
-        let mut doc = self.arena.nil();
+        Some((printed, col_widths))
+    }
 
+    fn print_aligned_cells(
+        &'a self,
+        printed: Vec<Vec<(String, usize)>>,
+        col_widths: Vec<usize>,
+    ) -> ArenaDoc<'a> {
         /*
         printed as:
           aa & bbbb && cccc \
         dddd & e    && f    \
          */
+        let mut doc = self.arena.nil();
+
         let row_num = printed.len();
         let col_num = col_widths.len();
         for (i, row) in printed.into_iter().enumerate() {
@@ -73,13 +96,50 @@ impl<'a> PrettyPrinter<'a> {
                 let col_width = col_widths[j];
                 let is_cur_empty = cell_width == 0;
 
-                let pad_spaces = self.arena.spaces(col_width - cell_width);
-                let cell_doc = self.arena.text(cell);
-                #[allow(clippy::if_same_then_else)]
-                let padded_cell_doc = if j % 2 == 1 || col_widths.len() == 1 {
-                    cell_doc + pad_spaces
+                let padded_cell_doc = if cell.has_linebreak() {
+                    let mut indent = col_widths[..j].iter().sum::<usize>() + j;
+                    if j > 0 {
+                        indent += 1;
+                    }
+                    self.arena
+                        .intersperse(
+                            cell.lines()
+                                .filter_map(|line| {
+                                    let trimmed = line.trim_ascii();
+                                    if trimmed.is_empty() {
+                                        None
+                                    } else {
+                                        Some(trimmed)
+                                    }
+                                })
+                                .map(|line| {
+                                    let render_width = line.width();
+                                    let line_width = if j == 0 || j + 1 == col_num {
+                                        render_width + 1
+                                    } else {
+                                        render_width + 2
+                                    };
+                                    let pad_spaces = self.arena.spaces(col_width - line_width);
+                                    let line_doc = self.arena.text(line.to_string());
+                                    #[allow(clippy::if_same_then_else)]
+                                    if j % 2 == 1 || col_widths.len() == 1 {
+                                        line_doc + pad_spaces
+                                    } else {
+                                        pad_spaces + line_doc
+                                    }
+                                }),
+                            self.arena.hardline(),
+                        )
+                        .nest(indent as isize)
                 } else {
-                    pad_spaces + cell_doc
+                    let pad_spaces = self.arena.spaces(col_width - cell_width);
+                    let cell_doc = self.arena.text(cell);
+                    #[allow(clippy::if_same_then_else)]
+                    if j % 2 == 1 || col_widths.len() == 1 {
+                        cell_doc + pad_spaces
+                    } else {
+                        pad_spaces + cell_doc
+                    }
                 };
 
                 let sep = {
@@ -115,8 +175,7 @@ impl<'a> PrettyPrinter<'a> {
                 doc += self.arena.hardline();
             }
         }
-
-        Some(doc)
+        doc
     }
 }
 
