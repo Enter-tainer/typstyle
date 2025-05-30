a948968c1523871ca5a6653c35d323f09c3de640
QuadnucYard <quadnucyard@gmail.com>
2025-05-30 10:11:02 +0800

    dev: manual chunk

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -9,4 +9,31 @@ export default defineConfig({
   base: "/typstyle/playground/",
 
   plugins: [react(), tailwindcss(), wasm(), toplevelAwait()],
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id): string | undefined => {
+          // Large packages get their own chunks
+          if (id.includes("monaco-editor")) {
+            return "monaco-editor";
+          }
+          if (id.includes("monaco-themes")) {
+            return "monaco-themes";
+          }
+          if (id.includes("react")) {
+            return "react";
+          }
+
+          // Group all application source code and public resources together
+          if (id.includes("/src/") || id.includes("?url")) {
+            return "app";
+          }
+
+          // Default chunk for everything else
+          return "vendor";
+        },
+      },
+    },
+  },
 });
